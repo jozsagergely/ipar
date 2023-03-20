2bdf1f03ec8fc770038541cbbf43f28f30d51db3
Józsa Gergely <jgergely2006vgmail.com>
2023-03-20 09:19:09 +0100

    TOROLVE

--- ipar1.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-  let menu = document.querySelector('.menu')
-  let toggle = document.querySelector('.toggle')
-  toggle.onclick = function() {
-    menu.classList.toggle('active')
-  }
-});
